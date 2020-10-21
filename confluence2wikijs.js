95e2d15faba4a4c3fdf9d437abe1dda55b07aa0b
Guy Kaplan <gkpln3@gmail.com>
2020-10-21 11:16:55 +0300

    Initial commit

---
+++ confluence2wikijs.js
@@ -0,0 +1,33 @@
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+var HTMLParser = require('node-html-parser');
+
+const directoryPath = process.argv[2];
+const outDirectoryPath = process.argv[3];
+fs.mkdirSync(outDirectoryPath)
+
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+
+    //listing all files using forEach
+    files.forEach(function (file) {
+        if (file.endsWith(".html")) {
+            fs.readFile(path.join(directoryPath, file), "utf8", function(err, data) {
+                parsedHtml = HTMLParser.parse(data)
+
+                page_title = parsedHtml.querySelector("head").querySelector("title").rawText
+                page_title = page_title.substr(page_title.search(" : ") + 3)
+                page_filename = page_title.replace(/ /g, "-");
+                
+                page_data = "<!--\ntitle: " + page_title + "\n-->"
+                page_data = page_data + parsedHtml.querySelector("#main-content").innerHTML;
+                
+                fs.writeFileSync(path.join(outDirectoryPath, page_filename + ".html"), page_data)
+            });
+        }
+    });
+});
